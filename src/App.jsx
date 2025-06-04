310a0c6efee1f0118b0b8c5c6948b80576d916ea
magdalena-nowak <magdalena.nowak@cytiva.com>
2025-06-04 09:57:26 +0200

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound"; // Add this import
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} /> {/* Catch-all route */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,12 @@
+import styles from "./Map.module.css";
+
+function Map() {
+  return (
+    <div className={styles.mapContainer}>
+      <h2>Map Component</h2>
+      {/* Map display goes here */}
+    </div>
+  );
+}
+
+export default Map;
